b5fafd3d690da974bd74a9a6d522b6a9ff0774cf
AaronBriggs <aaron00.briggs@gmail.com>
2020-11-02 15:28:44 -0600

    adds nonsense route to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/photos" => "photos#index"
+    #get "/routes" => "methods#controllermethod"
   end
 end

b436d67aa13992693c8fccdf6164720d679c4c25
AaronBriggs <aaron00.briggs@gmail.com>
2020-11-04 13:45:00 -0600

    finishing controller for education

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    get "/educations" => "educations#index"
+    get "/educations/:id" => "educations#show"
+    post "/educations" => "educations#create"
+    patch "/educations/:id" => "educations#update"
+    delete "/educations/:id" => "educations#destroy"
   end
 end

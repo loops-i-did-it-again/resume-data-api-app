5abe821688625408bd93a44ea47bf0f63a1d3cac
AaronBriggs <aaron00.briggs@gmail.com>
2020-11-04 12:52:45 -0600

    test

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -1,2 +1,17 @@
 class Api::StudentsController < ApplicationController
+  def index
+    render json: {message: index}
+  end
+  def show
+    render json: {message: show}
+  end
+  def create
+    render json: {message: create}
+  end
+  def update
+    render json: {message: update}
+  end
+  def destroy
+    render json: {message: destroy}
+  end
 end

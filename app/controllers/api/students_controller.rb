4b02bce1d3041885082f61aaa0c0449733eb71c8
Michael White <mwhite7372@gmail.com>
2020-11-05 13:26:32 -0600

    added update and destory routes to students controller

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -4,11 +4,6 @@ class Api::StudentsController < ApplicationController
     render "index.json.jb"
   end
 
-  def show
-    @student = Student.find(params[:id])
-    render "show.json.jb"
-  end
-
   def create
     student = Student.new(
       first_name: params[:first_name],
@@ -30,5 +25,42 @@ class Api::StudentsController < ApplicationController
     else
       render json: { errors: student.errors.full_messages }, status: :bad_request
     end
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @student = Student.find(params[:id])
+
+    @student.first_name = params[:first_name] || @student.first_name
+    @student.last_name = params[:last_name] || @student.last_name
+    @student.email = params[:email] || @student.email
+    @student.phone_number = params[:phone_number] || @student.phone_number
+    @student.bio = params[:bio] || @student.bio
+    @student.linkedin_url = params[:linkedin_url] || @student.linkedin_url
+    @student.twitter_url = params[:twitter_url] || @student.twitter_url
+    @student.personal_url = params[:personal_url] || @student.personal_url
+    @student.resume_url = params[:resume_url] || @student.resume_url
+    @student.github_url = params[:github_url] || @student.github_url
+    @student.photo_url = params[:photo_url] || @student.photo_url
+    if params[:password]
+      @student.password = params[:password]
+      # @student.password_confirmation = password_confirmation[:password_confirmation]
+    end
+
+    if @student.save
+      render "show.json.jb"
+    else
+      render json: { errors: @student.errors.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    student = Student.destroy(params[:id])
+    student.destroy
+    render json: { message: "The user has been deleted!" }
   end
 end
